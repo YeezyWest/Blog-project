afb3a223202730cfeca770655b8f18a9efdd0c37
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2022-10-30 22:04:02 +0100

    deploying the blog

---
+++ index.js
@@ -0,0 +1,8 @@
+let clickMe = document.getElementById("click-me");
+
+function openMenu() {
+  alert ( "oops! Sorry, You Can't Sign up Just Yet!");
+}
+function readMenu() {
+    alert ( "Hold Up! Nahhh! Just Messing With You!");
+  }
